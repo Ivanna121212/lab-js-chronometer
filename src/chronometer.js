5f763fd8a7cab85626e5682e580685e81d603be3
Anna Ivanivna <115419490+Ivanna121212@users.noreply.github.com>
2023-07-25 17:06:08 +0200

    chronometer.js

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,30 +1,46 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+   this.currentTime = 0;
+   this.intervalId = null;
+   this.clearInterval;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime += 1;
+      if (printTimeCallback){
+        printTimeCallback();
+      }
+    }, 1000);
   }
 
   getMinutes() {
-    // ... your code goes here
+    let minutes = this.currentTime / 60;
+    return Math.floor(minutes);
   }
 
   getSeconds() {
-    // ... your code goes here
+    let seconds = this.currentTime % 60;
+    return seconds;
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    value=value.toString();
+    if (value.length === 1){
+      return "0" + value;
+    } else {
+      return value;
+    }
   }
 
   stop() {
-    // ... your code goes here
+  this.clearInterval = this.intervalId; 
+  clearInterval();
+   
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
